ab5707c3dc449b95f1a142351837cbb11f5800b0
Gabriel Grant <g@briel.ca>
2011-11-09 19:00:54 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-secret-santa',
-    version='0.1.0dev',
+    version='0.1.1dev',
     author='Gabriel Grant',
     packages=['secret_santa'],
     license='LGPL',
