94629a1166b59d88f01a8cd44cd02a2a66afce77
Gabriel Grant <g@briel.ca>
2011-11-10 11:30:21 -0800

    Person model method fixes

--- secret_santa/models.py
+++ secret_santa/models.py
@@ -28,20 +28,33 @@ class Person(models.Model):
 		return self.name
 	def get_allowed_families(self):
 		family_giving_counts = self.family.get_family_giving_counts()
-		family_giving_counts.pop(None)
-		family_giving_counts.pop(self.family)
-		min_count = min(count for family, count in family_giving_counts.iteritems())
-		return set(family for family, count in family_giving_counts.iteritems() if count == min_count)
-			
-	def get_receiver(self):
-		allowed_families = self.get_allowed_families()
-		receiver = Person.objects.filter(
-			key=None,
-			family__in=allowed_families,
-		).order_by('?')[:0]
-		if len(receiver) == 1:
-			return receiver
+		family_giving_counts.pop(self.family, None)
+		max_count = max(count for family, count in family_giving_counts.iteritems())
+		allowed = set(family for family, count in family_giving_counts.iteritems() if count < max_count)
+		if allowed:
+			return allowed
 		else:
+			return set(family_giving_counts.iterkeys())
+			
+	def get_receiver(self, key):
+		if self.chosen_family is not None:
+			try:
+				return Person.objects.get(key=key)
+			except Person.DoesNotExist:
+				raise ValueError('Wrong key')
+		allowed_families = self.get_allowed_families()
+		receiver = Person.objects.filter(  #TODO this should use 1.4's 'select_for_update'
+			key='',
+			family__in=allowed_families,
+		).order_by('?')[:1]
+		if len(receiver) != 1:
 			raise RuntimeError('No eligible receivers found')
+		else:
+			receiver = receiver[0]
+		self.chosen_family = receiver.family
+		receiver.key = key
+		self.save()
+		receiver.save()
+		return receiver
 
 
