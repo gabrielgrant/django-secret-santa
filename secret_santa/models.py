43f3036f7e24716b9ec1858fa644a200508b42af
Gabriel Grant <g@briel.ca>
2011-11-10 11:15:27 -0800

    fix family stringification

--- secret_santa/models.py
+++ secret_santa/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Family(models.Model):
 	name = models.CharField(max_length=255, unique=True)
 	def __unicode__(self):
-		return self.name + ': ' + ', '.join(m.name for m in self.members.all())
+		return self.name
 	def get_family_giving_counts(self):
 		""" Returns the number of members of this family giving to each other family """
 		# note: "order_by()" is needed to exclude default ordering
