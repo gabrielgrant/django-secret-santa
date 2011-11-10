379fd78ad7846678f64a669b2ff806a050286164
Gabriel Grant <g@briel.ca>
2011-11-09 16:32:12 -0800

    add setup.py and README

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-secret-santa',
+    version='0.1.0dev',
+    author='Gabriel Grant',
+    packages=['secret_santa'],
+    license='LGPL',
+    long_description=open('README').read(),
+    install_requires=[
+        'django',
+    ],
+)
