c335be0c468c658337e365ba5df0bd609a710178
Gabriel Grant <g@briel.ca>
2011-11-09 19:27:25 -0800

    fix typo in utils.py

--- secret_santa/utils.py
+++ secret_santa/utils.py
@@ -2,5 +2,5 @@ from hashlib import sha1
 
 def make_key(name, password):
 	unhashed_key = ':'.join([name, password])
-	key = sha1(unhahsed_key).hexdigest()
+	key = sha1(unhashed_key).hexdigest()
 	return key
