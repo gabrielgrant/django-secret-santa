c9ec2b14ccd6aac1013d116b0bdb9a1359098fd6
Gabriel Grant <g@briel.ca>
2011-11-16 02:45:27 -0800

    add a static files directory

--- example_project/project/settings.py
+++ example_project/project/settings.py
@@ -75,6 +75,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    rootify('static_dir'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
