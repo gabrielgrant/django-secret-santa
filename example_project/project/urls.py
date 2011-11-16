9a0b9f090b2bd2f89318d3c8f2a9c6d03e56b8e4
Gabriel Grant <g@briel.ca>
2011-11-16 02:50:21 -0800

    add static about and contact pages

--- example_project/project/urls.py
+++ example_project/project/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', include('secret_santa.urls')),
-
+	url(r'^about/', TemplateView.as_view(template_name='about.html')),
+	url(r'^contact/', TemplateView.as_view(template_name='contact.html')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
